5412393d81f560939029778e24806d321f57ceb2
Jake Rose <jacob.rose@mymail.champlain.edu>
2019-10-20 17:19:26 -0400

    fixed connection menu

--- Sk8 Game/Assets/Scripts/MenuScripts/ConnectionMenu.cs
+++ Sk8 Game/Assets/Scripts/MenuScripts/ConnectionMenu.cs
@@ -2,14 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
 
 public class ConnectionMenu : MonoBehaviour
 {
     public TextMeshProUGUI ipText;
+    public Button submitButton;
     // Start is called before the first frame update
     void Start()
     {
-        
+        submitButton.onClick.AddListener(ConnectToIP);
     }
 
     // Update is called once per frame
