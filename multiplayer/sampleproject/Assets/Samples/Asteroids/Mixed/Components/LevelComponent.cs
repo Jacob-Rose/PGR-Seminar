30391019916857cd9895b3857b55fbd73fd43cf0
Jake Rose <jacob.rose@mymail.champlain.edu>
2019-11-07 14:59:48 -0500

    removed old folders

--- multiplayer/sampleproject/Assets/Samples/Asteroids/Mixed/Components/LevelComponent.cs
+++
@@ -1,7 +0,0 @@
-using Unity.Entities;
-
-public struct LevelComponent : IComponentData
-{
-    public int width;
-    public int height;
-}
--- multiplayer/sampleproject/Assets/Samples/Asteroids/Mixed/Components/VelocityComponent.cs
+++
@@ -1,7 +0,0 @@
-using Unity.Entities;
-using Unity.Mathematics;
-
-public struct Velocity : IComponentData
-{
-    public float2 Value;
-}
