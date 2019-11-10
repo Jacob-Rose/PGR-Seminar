cdee002c7972f19a5b9f66743b71a5eb8773cccc
Jake Rose <jacob.rose@mymail.champlain.edu>
2019-11-10 15:29:10 -0500

    finished road and wall generation, color determined by gradient for walls

--- Sk8 Game/Assets/Scripts/TileManager.cs
+++ Sk8 Game/Assets/Scripts/TileManager.cs
@@ -12,10 +12,13 @@ public class TileManager : MonoBehaviour
     //just a sprite for the road
     public GameObject m_RoadPrefab;
     public GameObject m_GrassPrefab;
+    public Gradient m_WallGradient;
+    public GameObject m_WallPrefab;
 
     public int roadCount = 300;
     public Vector2 desiredRoadTileSize = new Vector2(8.0f, 12.0f);
     public float m_GrassWidth = 4.0f;
+    public float m_WallWidth = 2.0f;
     //private Vector2 roadTileSize;
     public int obstaclesPerTile = 2;
 
@@ -37,8 +40,9 @@ public class TileManager : MonoBehaviour
         m_GrassPrefab.transform.localScale = new Vector3(m_GrassWidth / spriteBounds.size.x, desiredRoadTileSize.y / spriteBounds.size.y, 1) ;
         spriteBounds = m_RoadPrefab.GetComponent<SpriteRenderer>().sprite.bounds;
         m_EndPrefab.transform.localScale = new Vector3(desiredRoadTileSize.x / spriteBounds.size.x, 6.0f / spriteBounds.size.y, 1);
-
-        PopulateRoads();
+        spriteBounds = m_WallPrefab.GetComponent<SpriteRenderer>().sprite.bounds;
+        m_WallPrefab.transform.localScale = new Vector3(m_WallWidth/ spriteBounds.size.x, desiredRoadTileSize.y / spriteBounds.size.y, 1);
+        PopulateRoads(); //to replace
         
     }
 
@@ -96,7 +100,11 @@ public class TileManager : MonoBehaviour
         //Spawn Road Item
         GameObject newRoad = Instantiate(m_RoadPrefab, spawnBounds.center, Quaternion.identity, transform);
         GameObject grassLeft = Instantiate(m_GrassPrefab, spawnBounds.center + new Vector3((spawnBounds.size.x + m_GrassWidth)* 0.5f , 0,0), Quaternion.identity, transform);
-        GameObject grassRight = Instantiate(m_GrassPrefab, spawnBounds.center - new Vector3((spawnBounds.size.x + m_GrassWidth) * 0.5f, 0,0), Quaternion.identity, transform);
+        GameObject grassRight = Instantiate(m_GrassPrefab, spawnBounds.center - new Vector3((spawnBounds.size.x + m_GrassWidth) * 0.5f, 0, 0), Quaternion.identity, transform);
+        GameObject wallLeft = Instantiate(m_WallPrefab, spawnBounds.center - new Vector3((spawnBounds.size.x + (m_GrassWidth * 2) + m_WallWidth) * 0.5f, 0, 0), Quaternion.identity, transform);
+        wallLeft.GetComponent<SpriteRenderer>().color = m_WallGradient.Evaluate(((float)m_CurrentRoadCount) / roadCount);
+        GameObject wallRight = Instantiate(m_WallPrefab, spawnBounds.center + new Vector3((spawnBounds.size.x + (m_GrassWidth * 2) + m_WallWidth) * 0.5f, 0, 0), Quaternion.identity, transform);
+        wallRight.GetComponent<SpriteRenderer>().color = m_WallGradient.Evaluate(((float)m_CurrentRoadCount) / roadCount);
         if (m_CurrentRoadCount >= roadCount - 3) //spawn end for the last three roads
         {
             Instantiate(m_EndPrefab, spawnBounds.center, Quaternion.identity, transform);
